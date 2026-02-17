3351c9705584293487aeb9d91875c9935f9d4759
Developer <developer@example.com>
2026-02-17 07:45:55 -0600

    feat: force chrome open in start script

--- INICIAR_ROBO.bat
+++ INICIAR_ROBO.bat
@@ -10,9 +10,9 @@ cd /d "%~dp0"
 echo [1/2] Ligando o MOTOR do Robo (Necessario para operar)...
 start "MOTOR CRYPTUM - NAO FECHE" cmd /k "npm run start:bot"
 
-echo [2/2] Acessando o Painel na NUVEM (Vercel)...
+echo [2/2] Acessando o Painel na NUVEM (Vercel) via CHROME...
 timeout /t 3 >nul
-start https://nexus-crypto-bot.vercel.app
+start chrome https://nexus-crypto-bot.vercel.app
 
 echo.
 echo ==================================================
